4c1105747ee1c92f4841be3328b96d3cd71bb7f0
AnnaBratchenia <grigorevichanna@gmail.com>
2022-09-02 14:45:52 +0300

    introducing_vue_components

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-const app = Vue.createApp({
+const app = Vue.createApp({});
+
+app.component("friend-contact", {
+  template: `
+  <li class="list_items box">
+            <div class="name">{{friend.name}}</div>
+            <button v-on:click="toggleButton()" class="btn">
+              {{detaisIsShowed? 'Hide':'Show'}} details
+            </button>
+            <ul v-if="detaisIsShowed">
+              <div class="text box">Phone: {{friend.phone}}</div>
+              <div class="text box">Email: {{friend.email}}</div>
+            </ul>
+          </li>
+  `,
   data() {
     return {
-      friends: [
-        {
-          id: "anna",
-          name: "Anna Mull",
-          phone: "0 123 456 789",
-          email: "annaM@gmail.com",
-        },
-        {
-          id: "john",
-          name: "John Scwarzkopf",
-          phone: "0 987 654 321",
-          email: "scwarzkopf@gmail.com",
-        },
-        {
-          id: "willi",
-          name: "William Borman",
-          phone: "0 123 654 987",
-          email: "bormanWM@gmail.com",
-        },
-      ],
       detaisIsShowed: false,
+      friend: {
+        id: "willi",
+        name: "William Borman",
+        phone: "0 123 654 987",
+        email: "bormanWM@gmail.com",
+      },
     };
   },
-
   methods: {
     toggleButton() {
       this.detaisIsShowed = !this.detaisIsShowed;
